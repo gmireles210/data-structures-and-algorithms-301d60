5c4ac023120be36eb286bc4eab61dd54e70969d2
Greg M <44076044+gmireles210@users.noreply.github.com>
2020-01-08 14:18:36 -0800

    Update ArrayShift.java

--- code401challenges/src/main/ArrayShift.java
+++ code401challenges/src/main/ArrayShift.java
@@ -3,27 +3,11 @@ package code401challenges;
 import java.util.ArrayList;
 
 public class ArrayShift {
-    public static int[] insertShiftArray(int[] arrayToBeShifted, int valueToBeAdded) {
-        int arrayMidPoint = arrayToBeShifted.length / 2;
-        int[] result= new int[arrayToBeShifted.length +1];
-        if(arrayToBeShifted.length % 2 == 0) {
-            for (int i = 0; i< result.length; i++) {
-                if(i < arrayMidPoint){
+    public static ArrayList<Integer> insertShiftArray(ArrayList<Integer> arrayToBeShifted, int valueToBeAdded) {
+        float arrayMidPoint = arrayToBeShifted.size() / 2;
 
-                }
-                else if ( i == arrayMidPoint) {
-                    // this is where valueToBeAdded goes
-                    arrayToBeShifted.length, arrayMidPoint, valueToBeAdded
-                }
-                else{
-                    // copy over second half of input array
-                    arrayToBeShifted.length +1,
-
-                }
-
-
-            }
-
+        if(arrayToBeShifted.size() % 2 == 0) {
+            arrayToBeShifted.add((int) arrayMidPoint, valueToBeAdded);
         } else {
             arrayToBeShifted.add((int) ((arrayMidPoint + .5) + 1), valueToBeAdded);
         }
