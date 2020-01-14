11c45722fb558dfb1ca04176a0fa31bd2b11a8ae
Gregory Mireles <gmireles210@gmail.com>
2020-01-14 12:22:23 -0800

    finished code6 and tests

--- code401challenges/src/main/java/linkedList/LinkedList.java
+++ code401challenges/src/main/java/linkedList/LinkedList.java
@@ -1,52 +1,133 @@
 package linkedList;
 
-public class LinkedList {
-    // makes Node head seen to entire file
-    Node head;
-    // Node pointer to the next
-    class Node {
-        int dataVal;
-        Node next;
-        Node(int dataVal, Node next) {
-            this.dataVal = dataVal;
-            this.next = next;
+//Made LinkedList Public so it can be viewed throughout
+//Named LL Potatoe to try using a diff variable
+
+public class LinkedList<Potatoe> {
+
+// methods learned from class
+    Node<Potatoe> head;
+    //refactored old code using same names in order to create Node var
+    static int dataVal;
+
+    public LinkedList() {
+        this.head = null;
+        dataVal = 0;
+    }
+
+    public void insertHead(Potatoe value) {
+
+        if (this.head == null) {
+            this.head = new Node<>(value);
+        } else {
+            Node<Potatoe> newNode1 = new Node<>(value);
+            newNode1.followOn = this.head;
+            this.head = newNode1;
+        }
+        LinkedList.dataVal++;
+    }
+
+    public void append(Potatoe value) {
+
+        if (this.head == null) {
+            head = new Node<>(value);
+        } else {
+            Node<Potatoe> newNode = new Node<>(value);
+            Node<Potatoe> curr3 = this.head;
+            while (curr3.followOn != null) {
+                curr3 = curr3.followOn;
+            }
+
+            curr3.followOn = newNode;
         }
     }
-    // Head
-    public void insertHead(int headValue){
-        // New Head instantiation
-        Node NodeHead = new Node(headValue, null);
-        // Retain old list data
-        Node FormerHead = this.head;
-        // Create new head
-        this.head = NodeHead;
-        // Assign new head's "next" to old list data
-        this.head.next = FormerHead;
-    }
-    // Include List with dataVal
-    public boolean includedInList(int searchValue){
-        // Creating current instance
-        Node curr = this.head;
-        while (curr != null){
-            if (searchValue == curr.dataVal){
+
+    public boolean insertBefore(Potatoe value, Potatoe newVal) {
+
+        Node<Potatoe> curr1 = this.head;
+
+        while (curr1.followOn != null) {
+
+            if (curr1.followOn.value.equals(value)) {
+                Node<Potatoe> newNode = new Node<>(newVal);
+                newNode.followOn = curr1.followOn;
+                curr1.followOn = newNode;
                 return true;
             }
-            curr = curr.next;
+
+            curr1 = curr1.followOn;
+        }
+
+        return false;
+    }
+
+    public boolean insertAfter(Potatoe value, Potatoe newVal) {
+
+        Node<Potatoe> curr2 = this.head;
+
+        while (curr2.followOn != null) {
+
+            if (curr2.value.equals(value)) {
+                Node<Potatoe> newNode = new Node<>(newVal);
+                newNode.followOn = curr2.followOn;
+                curr2.followOn = newNode;
+                return true;
+
+            }
+            curr2 = curr2.followOn;
+        }
+
+        return false;
+    }
+
+    public boolean includedInList(Potatoe value) {
+
+        Node<Potatoe> curr4 = this.head;
+
+        while (curr4 != null) {
+            if (curr4.value == value) {
+                return true;
+            } else {
+                curr4 = curr4.followOn;
+            }
         }
         return false;
     }
-    // Create string with dataVal
-    public String toString(){
 
-        Node curr = this.head;
-        String overallCommand = "";
-        while (curr != null){
-            String stringStatement = "{ " + curr.dataVal + " } => ";
-            overallCommand = overallCommand + stringStatement;
-            curr = curr.next;
-
-        }
-        return overallCommand + "null";
+    public int getSize() {
+        return dataVal;
     }
 
+
+    public String toString() {
+
+        StringBuilder listString = new StringBuilder();
+
+        Node<Potatoe> curr5 = head;
+
+        while (curr5 != null) {
+            listString.append("{ ");
+            listString.append(curr5.value.toString());
+
+            if (curr5.followOn == null) {
+                listString.append(" } -> NULL");
+            } else {
+                listString.append(" } -> ");
+            }
+            curr5 = curr5.followOn;
+        }
+
+        return listString.toString();
+    }
+
+    public static class Node<Potatoe2> {
+
+        public Potatoe2 value;
+        public Node<Potatoe2> followOn;
+
+        public Node(Potatoe2 value) {
+            this.value = value;
+            this.followOn = null;
+        }
+    }
 }
